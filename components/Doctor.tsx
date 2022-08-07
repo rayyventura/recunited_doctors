c458ec0ae241e18fd1c6aed43b70e8bfe63d255e
Rayane Ventura <rayyventura@gmail.com>
2022-08-07 20:04:02 +0200

    adjust

--- components/Doctor.tsx
+++ components/Doctor.tsx
@@ -56,10 +56,10 @@ export default function Doctor({
         className="object-cover w-56 h-56 mb-1 rounded-md hover:opacity-70"
       />
       <p className="font-bold text-dark-blue">
-        {firstName} {lastName}, {facility}
+        {firstName} {lastName}, {city}
       </p>
       <p className=" text-dark-blue">
-        {areaOfExpertise}, {city}
+        {areaOfExpertise}, {facility}
       </p>
       <p className="text-sky-900 opacity-80 hover:opacity-100">{email}</p>
       <div className="flex flex-row">
